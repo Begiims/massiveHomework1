b226098cf9ae47d53a94e38a26bf283e82a6e7ca
Begiims <attokurovabegimaj@gmail.com>
2022-12-09 19:40:32 +0300

    hjnioj

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hgj");
         System.out.println("Hello worlds");
+        System.out.println("Hello world");
     }
 }
